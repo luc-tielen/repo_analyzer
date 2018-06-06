136a0d92156acae322a1f018becb975a58e26561
Luc Tielen <luc.tielen@kabisa.nl>
2018-06-06 20:46:00 +0200

    Add function for analyzing diff stats in a file

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -3,37 +3,69 @@
 mod tests;
 
 use std::vec::Vec;
-use git2::{Error,Repository,Oid,Sort};
+use git2::{Error,Repository,Oid,Sort,DiffOptions,Time,Signature};
 
 // TODO optimize operations on collections
 
-pub fn analyze_repo<'a>(repo_dir: &'a str) -> Vec<Commit<'a>> {
-    vec![]
+pub struct Delta {
+    hash: Oid,
+    summary: String,
+    file_name: String,
+    time: Time,
+    author: String,
+    additions: usize,
+    deletions: usize,
 }
 
-pub fn list_commits<'a>(repo_dir: &'a str) -> Result<Vec<Oid>, Error> {
-    let repo = Repository::open(repo_dir)?;
+pub fn list_commits<'a>(repo: &'a Repository) -> Result<Vec<Oid>, Error> {
     let mut revwalk = repo.revwalk()?;
     revwalk.set_sorting(Sort::TOPOLOGICAL | Sort::TIME);
     revwalk.push_head()?;
 
+    // TODO use iterator for more optimal code
     let mut result = vec![];
     for rev in revwalk {
-        let id = rev?;
-        println!("{}", id);
-        result.push(id);
+        let oid = rev?;
+        // skip merge commits in analysis:
+        if repo.find_commit(oid)?.parent_ids().len() > 1 {
+            continue;
+        }
+        result.push(oid);
     }
+    result.reverse();
     Ok(result)
 }
 
-pub struct Commit<'a> {
-    hash: &'a str,
-    description: &'a str,
-    diffs: Vec<Diff<'a>>,
+pub fn extract_diff_info<'a>(repo: &'a Repository, oid: Oid,
+                             file: String) -> Result<Delta, Error> {
+    let mut diff_opts = DiffOptions::new();
+    let diff_opts = Some(diff_opts.pathspec(file.clone()));
+    let commit = repo.find_commit(oid)?;
+    let tree = commit.tree()?;
+    let diff = match commit.parents().count() {
+        0 => {  // Starting commit has no parent
+            repo.diff_tree_to_tree(None, Some(&tree), diff_opts)?
+        },
+        _ => {
+            let parent_commit = commit.parent(0)?;
+            let parent_tree = parent_commit.tree()?;
+            repo.diff_tree_to_tree(Some(&parent_tree), Some(&tree), diff_opts)?
+        },
+    };
+
+    let stats = diff.stats()?;
+    let hash = commit.id();
+    let msg = commit.message().unwrap().to_string();
+    let time = commit.time();
+    let author = commit.author().name().unwrap().to_string();
+    Ok(Delta {
+        hash: hash,
+        summary: msg,
+        file_name: file,
+        time: time,
+        author: author,
+        additions: stats.insertions(),
+        deletions: stats.deletions(),
+    })
 }
 
-pub struct Diff<'a> {
-    filename: &'a str,
-    additions: u64,
-    deletions: u64,
-}
--- src/analyzer/tests.rs
+++ src/analyzer/tests.rs
@@ -1,22 +1,49 @@
 
 use std::vec::Vec;
-use analyzer::list_commits;
-use git2::Error;
+use analyzer::{list_commits,extract_diff_info};
+use git2::{Error,Repository,DiffFormat};
 
 
 #[test]
 fn listing_commits() {
-    match list_commits("./fixtures") {
+    let repo = Repository::open("./fixtures").unwrap();
+    match list_commits(&repo) {
         Ok(commits) => {
             let commit_strings: Vec<String> =
                 commits.iter().map(|c| format!("{}", c)).collect();
             let expected: Vec<&str> = vec![
-                "19a6ca15ecb29c4debd5b32181c3f7af22b05923",
+                "5a88fa35c3e589537ff04ffa4bd8203409fe6e36",
                 "20d2b7e9858b02a658eee11dfbab2fc8a3f1c900",
-                "5a88fa35c3e589537ff04ffa4bd8203409fe6e36"
+                "19a6ca15ecb29c4debd5b32181c3f7af22b05923",
             ];
             assert_eq!(commit_strings, expected);
         }
         Err(e) => assert!(false)
     };
+}
+
+#[test]
+fn extracting_diff_info() {
+    let repo = Repository::open("./fixtures").unwrap();
+    let commits = list_commits(&repo).unwrap();
+    let commit1 = commits[0];
+    let commit2 = commits[1];
+    let commit3 = commits[2];
+
+    let diff1 = extract_diff_info(&repo, commit1, "file1".to_string()).unwrap();
+    let diff2_1 = extract_diff_info(&repo, commit2, "file1".to_string()).unwrap();
+    let diff2_2 = extract_diff_info(&repo, commit2, "file2".to_string()).unwrap();
+    let diff3_1 = extract_diff_info(&repo, commit3, "file1".to_string()).unwrap();
+    let diff3_2 = extract_diff_info(&repo, commit3, "file2".to_string()).unwrap();
+    assert_eq!(diff1.file_name, "file1");
+    assert_eq!(diff1.additions, 3);
+    assert_eq!(diff1.deletions, 0);
+    assert_eq!(diff2_1.additions, 1);
+    assert_eq!(diff2_1.deletions, 1);
+    assert_eq!(diff2_2.additions, 0);
+    assert_eq!(diff2_2.deletions, 0);
+    assert_eq!(diff3_1.additions, 1);
+    assert_eq!(diff3_1.deletions, 1);
+    assert_eq!(diff3_2.additions, 3);
+    assert_eq!(diff3_2.deletions, 0);
 }
